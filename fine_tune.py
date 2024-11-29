5a5704ccfdf6f1d8722b3858dd5aec4dbdda132e
ak <ahmedelkhedir9@gmail.com>
2024-11-29 19:22:05 +0400

    commit

--- fine_tune.py
+++ fine_tune.py
@@ -2,8 +2,8 @@ import openai
 
 openai.api_key = "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"
 
-fine_tune_response = openai.FineTune.create(
-    training_file="file-W7NZ4K4FPrtxwjNWC41qm1",  # Your file ID
-    model="curie"  # Base model for fine-tuning
+response = openai.FineTuningJob.create(
+    training_file="file-W7NZ4K4FPrtxwjNWC41qm1",
+    model="gpt-4o-mini-2024-07-18"
 )
-print("File uploaded successfully. File ID:", fine_tune_response["id"])
+print(response)
