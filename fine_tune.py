d31ba4e62092807203a57694d44ce1727f92db67
ak <ahmedelkhedir9@gmail.com>
2025-02-26 15:04:14 +0400

    me

--- fine_tune.py
+++ fine_tune.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 response = openai.FineTuningJob.create(
     training_file="file-AFKRtmMTDiEaWjnvMGaA3p",
